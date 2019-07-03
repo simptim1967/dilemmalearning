7f0a8cd6b6bc04cf98720d737c684c538369d593
Tim Simpson <simptim1967@users.noreply.github.com>
2019-07-03 11:18:29 +0100

    New personal learning trick 06

--- pug-tricks.bat
+++ pug-tricks.bat
@@ -10,7 +10,7 @@ call Pug .\pug\tricks\personal\02-make-time-for-personal-learning.pug --pretty -
 call Pug .\pug\tricks\personal\03-hide-away-change-the-scenery.pug --pretty --out ..\live\tricks\personal
 call Pug .\pug\tricks\personal\04-avoid-distractions-and-occasional-interruptions.pug --pretty --out ..\live\tricks\personal
 call Pug .\pug\tricks\personal\05-create-a-personal-development-plan.pug --pretty --out ..\live\tricks\personal
-rem call Pug .\pug\tricks\personal\06-learn-to-manage-your-time-well.pug --pretty --out ..\live\tricks\personal
+call Pug .\pug\tricks\personal\06-learn-to-manage-your-time-well.pug --pretty --out ..\live\tricks\personal
 rem call Pug .\pug\tricks\personal\07-find-ways-of-learning-that-work-for-you.pug --pretty --out ..\live\tricks\personal
 rem call Pug .\pug\tricks\personal\08-join-a-community.pug --pretty --out ..\live\tricks\personal
 rem call Pug .\pug\tricks\personal\09-join-a-mailing-list.pug --pretty --out ..\live\tricks\personal
