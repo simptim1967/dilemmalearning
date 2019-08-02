666dcd187193201b65e4b176cc30326c1304cc58
Tim Simpson <simptim1967@users.noreply.github.com>
2019-08-02 09:10:53 +0100

    New weekly blog article

--- pug-blog.bat
+++ pug-blog.bat
@@ -17,4 +17,5 @@ call Pug .\pug\blog\a-time-to-reflect.pug --pretty --out ..\live\blog
 call Pug .\pug\blog\opening-new-doors-to-new-possibilities.pug --pretty --out ..\live\blog
 call Pug .\pug\blog\continuous-curiosity.pug --pretty --out ..\live\blog
 call Pug .\pug\blog\blast-off-from-the-past.pug --pretty --out ..\live\blog
+call Pug .\pug\blog\out-learn-the-competition.pug --pretty --out ..\live\blog
 rem call Pug .\pug\blog\xxx.pug --pretty --out ..\live\blog
