cf2842d78c3400f40c248256576ef98acf74d30c
Miliunas <al.miliunas@capgemini.com>
2019-08-29 16:26:52 +0100

    Cookie popup, privacy page, pug-test.bat

--- pug-test.bat
+++ pug-test.bat
@@ -1,5 +1,8 @@
 @echo off
 echo Rendering pug test files ...
+call cd dilemmalearning
+call cd
 call Pug pug\test\01-decide-what-to-learn.pug --pretty --out ..\test
 call Pug pug\test\personal-new-trick-list.pug --pretty --out ..\test
+call Pug pug\test\privacy.pug --pretty --out ..\test
 copy pagelike.js ..\test
