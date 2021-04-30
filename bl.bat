86b1354d2452520055212f3ca3fe64641b6563e1
Tim Simpson <simptim1967@users.noreply.github.com>
2021-04-30 10:35:39 +0100

    Latest blog article - Observing the invisible

--- bl.bat
+++ bl.bat
@@ -1,5 +1,6 @@
 @echo off
 echo Rendering latest blog article ...
+call Pug .\pug\blog\2021\33-observing-the-invisible.pug --pretty --out ..\live\blog\2021
 call Pug .\pug\blog\2021\32-designing-for-traceability.pug --pretty --out ..\live\blog\2021
 call Pug .\pug\blog\blog-history.pug --pretty --out ..\live\blog
 
