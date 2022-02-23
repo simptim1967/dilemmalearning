1977fa31ad9810df99cda6589860a9947a9598b2
Tim Simpson <simptim1967@users.noreply.github.com>
2022-02-23 12:54:09 +0000

    Blog article 37

--- bl.bat
+++ bl.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Rendering latest blog article ...
+call Pug .\pug\blog\2022\37-invest-in-product-backlog-refinement.pug --pretty --out ..\live\blog\2022
 call Pug .\pug\blog\2022\36-gaining-value-from-sprint-retrospectives.pug --pretty --out ..\live\blog\2022
-call Pug .\pug\blog\2021\35-spicing-up-sprint-reviews.pug --pretty --out ..\live\blog\2021
 call Pug .\pug\blog\blog-history.pug --pretty --out ..\live\blog
 
